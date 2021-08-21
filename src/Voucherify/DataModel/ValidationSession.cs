d8ef4e9d825afd695d9cb4fd2a85633b42f48762
Bandro <bandraszyk@gmail.com>
2021-08-21 23:24:57 +0200

    - Fill missing properties for ValidationSession (#77)

--- src/Voucherify/DataModel/ValidationSession.cs
+++ src/Voucherify/DataModel/ValidationSession.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+
 namespace Voucherify.DataModel
 {
+    [JsonObject]
     public class ValidationSession
     {
+        [JsonProperty(PropertyName = "key")]
+        public string Key { get; private set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; private set; }
+
+        [JsonProperty(PropertyName = "ttl")]
+        public string TTL { get; private set; }
+
+        [JsonProperty(PropertyName = "ttl_unit")]
+        public string TTLUnit { get; private set; }
+
         public ValidationSession()
         {
         }
